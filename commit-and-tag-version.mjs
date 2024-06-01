c4487ccbf57896f90520d1efc13887533c042809
Mara <mara-li@outlook.fr>
2024-06-01 09:40:10 +0200

    chore: use scripts for changelog generation

--- commit-and-tag-version.mjs
+++ commit-and-tag-version.mjs
@@ -4,26 +4,6 @@ import dedent from "dedent";
 import pkg from "ansi-colors";
 const { red, dim, gray, italic, bold, cyan, blue, green, underline, yellow, theme } = pkg;
 
-import { readFileSync, writeFile } from "fs";
-
-/**
- * Remove text from the file
- * @param {string} path
- */
-function removeText(path) {
-	const toRemove = [
-		"# Changelog",
-		"All notable changes to this project will be documented in this file. See [commit-and-tag-version](https://github.com/absolute-version/commit-and-tag-version) for commit guidelines.",
-	];
-	let changelog = readFileSync(path, "utf8");
-	for (const remove of toRemove) changelog = changelog.replace(remove, "").trim();
-	changelog = changelog.replaceAll(/[\n\r]{3,}/gm, "\n\n").trim();
-	changelog = changelog.replaceAll(/## (.*)[\n\r]{2}### /gm, "## $1\n### ").trim();
-	writeFile(path, changelog.trim(), "utf8", (err) => {
-		if (err) return console.error(err);
-	});
-}
-
 const program = new Command();
 
 theme({
@@ -48,7 +28,6 @@ program
 	.description("Bump version and create a new tag")
 	.option("-b, --beta", "Pre-release version")
 	.option("--dry-run", "Dry run")
-	.option("-c, --changelog", "Fix the changelog only")
 	.addOption(
 		new Option("-r, --release-as <size>", "release type version").choices([
 			"major",
@@ -60,100 +39,91 @@ program
 program.parse();
 const opt = program.opts();
 
-if (opt.changelog) {
-	const path = opt.beta ? "CHANGELOG-beta.md" : "CHANGELOG.md";
-	removeText(path);
-	console.log(`${heading("Changelog fixed")}: ${info(path)}`);
-	//wait for the file to be written
-	setTimeout(() => {
-		process.exit(0);
-	}, 1000);
-} else {
-	const betaMsg = opt.beta ? em("- Pre-release\n\t") : "";
-	const dryRunMsg = opt.dryRun ? em("- Dry run\n\t") : "";
-	const releaseAsMsg = opt.releaseAs
-		? em(`- Release as ${underline(opt.releaseAs)}`)
-		: "";
+const betaMsg = opt.beta ? em("- Pre-release\n\t") : "";
+const dryRunMsg = opt.dryRun ? em("- Dry run\n\t") : "";
+const releaseAsMsg = opt.releaseAs ? em(`- Release as ${underline(opt.releaseAs)}`) : "";
 
-	const msg = dedent(`
+const msg = dedent(`
 	${heading("Options :")}
 		${betaMsg}${dryRunMsg}${releaseAsMsg}  
 	`);
 
-	console.log(msg);
+console.log(msg);
+console.log();
+
+if (opt.beta) {
+	console.log(`${bold.green(">")} ${info.underline("Bumping beta version...")}`);
+	console.log();
+	const bumpFiles = [
+		{
+			filename: "manifest-beta.json",
+			type: "json",
+		},
+		{
+			filename: "package.json",
+			type: "json",
+		},
+		{
+			filename: "package-lock.json",
+			type: "json",
+		},
+	];
+	commitAndTagVersion({
+		infile: "CHANGELOG-beta.md",
+		bumpFiles,
+		prerelease: "",
+		dryRun: opt.dryRun,
+		tagPrefix: "",
+		scripts: {
+			postchangelog: "node _changelog.mjs -b",
+		},
+	})
+		.then(() => {
+			console.log("Done");
+		})
+		.catch((err) => {
+			console.error(err);
+		});
+} else {
+	const versionBumped = opt.releaseAs
+		? info(`Release as ${underline(opt.releaseAs)}`)
+		: info("Release");
+	console.log(`${bold.green(">")} ${underline(versionBumped)}`);
 	console.log();
 
-	if (opt.beta) {
-		console.log(`${bold.green(">")} ${info.underline("Bumping beta version...")}`);
-		console.log();
-		const bumpFiles = [
-			{
-				filename: "manifest-beta.json",
-				type: "json",
-			},
-			{
-				filename: "package.json",
-				type: "json",
-			},
-			{
-				filename: "package-lock.json",
-				type: "json",
-			},
-		];
-		commitAndTagVersion({
-			infile: "CHANGELOG-beta.md",
-			bumpFiles,
-			prerelease: "",
-			dryRun: opt.dryRun,
-			tagPrefix: "",
-		})
-			.then(() => {
-				if (!opt.dryRun) removeText("CHANGELOG-beta.md");
-				console.log("Done");
-			})
-			.catch((err) => {
-				console.error(err);
-			});
-		removeText("CHANGELOG-beta.md");
-	} else {
-		const versionBumped = opt.releaseAs
-			? info(`Release as ${underline(opt.releaseAs)}`)
-			: info("Release");
-		console.log(`${bold.green(">")} ${underline(versionBumped)}`);
-		console.log();
+	const bumpFiles = [
+		{
+			filename: "manifest-beta.json",
+			type: "json",
+		},
+		{
+			filename: "package.json",
+			type: "json",
+		},
+		{
+			filename: "package-lock.json",
+			type: "json",
+		},
+		{
+			filename: "manifest.json",
+			type: "json",
+		},
+	];
 
-		const bumpFiles = [
-			{
-				filename: "manifest-beta.json",
-				type: "json",
-			},
-			{
-				filename: "package.json",
-				type: "json",
-			},
-			{
-				filename: "package-lock.json",
-				type: "json",
-			},
-			{
-				filename: "manifest.json",
-				type: "json",
-			},
-		];
-
-		commitAndTagVersion({
-			infile: "CHANGELOG.md",
-			bumpFiles,
-			dryRun: opt.dryRun,
-			tagPrefix: "",
-			releaseAs: opt.releaseAs,
+	commitAndTagVersion({
+		infile: "CHANGELOG.md",
+		bumpFiles,
+		dryRun: opt.dryRun,
+		tagPrefix: "",
+		releaseAs: opt.releaseAs,
+		scripts: {
+			postchangelog: "node _changelog.mjs",
+		},
+	})
+		.then(() => {
+			console.log("Done");
 		})
-			.then(() => {
-				if (!opt.dryRun) removeText("CHANGELOG.md");
-				console.log("Done");
-			})
-			.catch((err) => {
-				console.error(err);
-			});
-	}
+		.catch((err) => {
+			console.error(err);
+		});
 }
